9654e2e62fe2aaedbb203b8c3c8663528ef42a29
ajayda24 <ajaydtrevor@gmail.com>
2021-10-27 17:40:44 +0530

    font error fixing

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
 import Head from 'next/head'
 import Script from 'next/script'
+import "@fontsource/comic-neue"
 
 import { useEffect } from 'react'
 
